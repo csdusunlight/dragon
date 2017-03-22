9b3230f6bf4211194d957bacb5e2afbd904de071
lch <18500581509@163.com>
2017-03-22 16:24:11 +0800

    rm coupons

--- wafuli_admin/management/commands/day-task.py
+++ wafuli_admin/management/commands/day-task.py
@@ -5,7 +5,7 @@ Created on 2016年8月29日
 @author: lch
 '''
 import logging
-from wafuli.models import Welfare, UserTask
+from wafuli.models import Welfare, UserTask, Coupon, CouponProject
 from django.core.management.base import BaseCommand
 from account.models import MyUser
 from django.db.models import F
@@ -22,5 +22,12 @@ class Command(BaseCommand):
             task.left_num = F("left_num")+1
             task.save(update_fields=['left_num'])
             usertask.delete()
+            
+        projects = CouponProject.objects.filter(endtime__lte=datetime.date.today()).order_by()
+        for project in projects:
+            if project.ctype == '2':
+                project.coupons.all().delete()
+            else:
+                project.coupons.filter(is_used=False).delete()
         end_time = time.time()
         logger.info("******Day-task is finished, time:%s*********",end_time-begin_time)
