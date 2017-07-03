54885830c5ecb4098ed23a793f5ac13c9b06f316
lch <18500581509@163.com>
2017-07-03 16:16:21 +0800

    restful

---
+++ project_admin/serializers.py
@@ -0,0 +1,13 @@
+#coding:utf-8
+'''
+Created on 2017年7月3日
+
+@author: lch
+'''
+from rest_framework import serializers
+from .models import Project
+class ProjectSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Project
+        fields = ('uuid', 'name', 'time', 'contact', 'coopway', 'settleway', 'state',
+                  'contract_company', 'settle_detail', 'paid_amount', 'consume_amount')
---
+++ project_admin/views.py
@@ -0,0 +1,26 @@
+#coding:utf-8
+from .models import Project
+from project_admin.serializers import ProjectSerializer
+# Create your views here.
+
+from rest_framework import generics, permissions
+from project_admin.permissions import IsAdminOrReadOnly,\
+    CsrfExemptSessionAuthentication
+import django_filters
+from project_admin.Filters import ProjectFilter
+class BaseViewMixin(object):
+    authentication_classes = (CsrfExemptSessionAuthentication,)
+    permission_classes = (permissions.IsAuthenticated,IsAdminOrReadOnly)
+    
+class ProjectList(BaseViewMixin,generics.ListCreateAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+    filter_class = ProjectFilter
+#     search_fields = ('=name', '=contact')
+
+
+class ProjectDetail(BaseViewMixin,generics.RetrieveUpdateDestroyAPIView):
+    queryset = Project.objects.all()
+    serializer_class = ProjectSerializer
+    
