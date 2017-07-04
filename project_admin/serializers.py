956b302424699c50e8a1a1c4f0721ba734bebdf1
lch <18500581509@163.com>
2017-07-04 10:24:06 +0800

    project

--- project_admin/serializers.py
+++ project_admin/serializers.py
@@ -7,8 +7,9 @@ Created on 2017年7月3日
 from rest_framework import serializers
 from .models import Project
 class ProjectSerializer(serializers.ModelSerializer):
+    state_des = serializers.CharField(source='get_state_display', read_only=True)
     class Meta:
         model = Project
-        fields = ('uuid', 'name', 'time', 'contact', 'coopway', 'settleway', 'state',
+        fields = ('uuid', 'name', 'time', 'contact', 'coopway', 'settleway', 'state','state_des',
                   'contract_company', 'settle_detail', 'paid_amount', 'consume_amount',
                   'return_amount', 'topay_amount', 'profit')
