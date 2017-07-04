4ead8d9ac3ad96d2827c97e992e8a098264680ea
lch <18500581509@163.com>
2017-07-04 15:13:19 +0800

    project

--- project_admin/serializers.py
+++ project_admin/serializers.py
@@ -5,12 +5,30 @@ Created on 2017年7月3日
 @author: lch
 '''
 from rest_framework import serializers
-from .models import Project
+from .models import *
 class ProjectSerializer(serializers.ModelSerializer):
     state_des = serializers.CharField(source='get_state_display', read_only=True)
+    settleway_des = serializers.CharField(source='get_settleway_display', read_only=True)
     class Meta:
         model = Project
-        fields = ('uuid', 'name', 'time', 'contact', 'coopway', 'settleway', 'state','state_des',
+        fields = ('uuid', 'name', 'time', 'contact', 'coopway', 'settleway', 'settleway_des', 'state','state_des',
                   'contract_company', 'settle_detail', 'paid_amount', 'consume_amount',
                   'return_amount', 'topay_amount', 'profit')
         read_only_fields = ('uuid',)
+
+class ProjectInvestDataSerializer(serializers.ModelSerializer):
+    projectname = serializers.CharField(source='project.name', read_only=True)
+    projectuuid = serializers.CharField(source='project.uuid', read_only=True)
+    state_des = serializers.CharField(source='get_state_display', read_only=True)
+    class Meta:
+        model = ProjectInvestData
+        fields =('project', 'projectname', 'projectuuid','invest_mobile','invest_time','invest_amount',
+                 'invest_term','settle_amount','return_amount','state','state_des','remark')
+
+class ProjectBalanceSerializer(serializers.ModelSerializer):
+    projectname = serializers.CharField(source='project.name', read_only=True)
+    projectuuid = serializers.CharField(source='project.uuid', read_only=True)
+    class Meta:
+        model = ProjectBalance
+        fields =('project', 'projectname', 'projectuuid','date','income','expenditure',
+                 'remark')
--- project_admin/views.py
+++ project_admin/views.py
@@ -1,17 +1,20 @@
 #coding:utf-8
 from .models import Project
-from project_admin.serializers import ProjectSerializer
+from project_admin.serializers import ProjectSerializer,\
+    ProjectInvestDataSerializer, ProjectBalanceSerializer
 # Create your views here.
 
 from rest_framework import generics, permissions
 from project_admin.permissions import IsAdminOrReadOnly,\
     CsrfExemptSessionAuthentication
 import django_filters
-from project_admin.Filters import ProjectFilter
+from project_admin.Filters import ProjectFilter, ProjectInvestDateFilter,\
+    ProjectBalanceFilter
 from django.shortcuts import redirect, render
 from django.core.urlresolvers import reverse
 from django.http.response import Http404
 from project_admin.Paginations import ProjectPageNumberPagination
+from project_admin.models import ProjectInvestData, ProjectBalance
 class BaseViewMixin(object):
     authentication_classes = (CsrfExemptSessionAuthentication,)
     permission_classes = (permissions.IsAuthenticated,IsAdminOrReadOnly)
@@ -28,7 +31,34 @@ class ProjectList(BaseViewMixin,generics.ListCreateAPIView):
 class ProjectDetail(BaseViewMixin,generics.RetrieveUpdateDestroyAPIView):
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
-   
+
+class ProjectInvestDataList(BaseViewMixin,generics.ListCreateAPIView):
+    queryset = ProjectInvestData.objects.all()
+    serializer_class = ProjectInvestDataSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+#     filter_fields = ('__all__')
+    filter_class = ProjectInvestDateFilter
+    pagination_class = ProjectPageNumberPagination
+#     search_fields = ('=name', '=contact')
+
+
+class ProjectInvestDataDetail(BaseViewMixin,generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProjectInvestData.objects.all()
+    serializer_class = ProjectInvestDataSerializer 
+
+class ProjectBalanceList(BaseViewMixin,generics.ListCreateAPIView):
+    queryset = ProjectBalance.objects.all()
+    serializer_class = ProjectBalanceSerializer
+    filter_backends = (django_filters.rest_framework.DjangoFilterBackend,)
+#     filter_fields = ('__all__')
+    filter_class = ProjectBalanceFilter
+    pagination_class = ProjectPageNumberPagination
+#     search_fields = ('=name', '=contact')
+
+
+class ProjectBalanceDetail(BaseViewMixin,generics.RetrieveUpdateDestroyAPIView):
+    queryset = ProjectBalance.objects.all()
+    serializer_class = ProjectBalanceSerializer 
 # 立项部分增加
 def project_index(request):
     admin_user = request.user
