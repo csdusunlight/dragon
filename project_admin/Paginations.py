1ecad16841c84d66fff2acb09be11c1f0729bb91
lch <18500581509@163.com>
2017-07-14 16:41:29 +0800

    pagesize

--- project_admin/Paginations.py
+++ project_admin/Paginations.py
@@ -9,6 +9,7 @@ from rest_framework.response import Response
 from collections import OrderedDict
 class ProjectPageNumberPagination(PageNumberPagination):
     page_size = 10
+    page_size_query_param = 'page_size'
     def get_paginated_response(self, data):
         if self.page.paginator.count>0:
             code = 1
