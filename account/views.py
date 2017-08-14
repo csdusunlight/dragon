faf38b4265fc5d25872c9555db26414032e48313
lvchunhui7 <lvchunhui7@126.com>
2017-08-14 19:25:42 +0800

    guide

--- account/views.py
+++ account/views.py
@@ -336,7 +336,8 @@ def account(request):
 
 def signin(request):
 
-
+    if not request.is_ajax():
+        raise Http404
 
     result={'code':-1, 'url':''}
     if not request.user.is_authenticated():
