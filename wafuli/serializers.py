070a3c4a47bce987e44a0328a8731c0bd1e9cbf2
lch <690501772@qq.com>
2017-08-23 12:03:56 +0800

    rest

--- wafuli/serializers.py
+++ wafuli/serializers.py
@@ -20,10 +20,11 @@ class MediaProjectSerializer(serializers.ModelSerializer):
         read_only_fields = ('id', 'pub_date')
 
 class UserEventSerializer(serializers.ModelSerializer):
-    project = serializers.CharField(source='content_object.title')
+    username = serializers.CharField(source='user.username', read_only=True)
+    project = serializers.CharField(source='content_object.title', read_only=True)
     state_des = serializers.CharField(source='get_audit_state_display', read_only=True)
     class Meta:
         model = UserEvent
         fields = '__all__'
         read_only_fields = ('id', 'audit_state', 'event_type', 'time', 'content_type',
-                            'object_id', 'user')
+                            'object_id', 'user', 'username', 'project')
