0727a173b758c66702b01faea54b8ef204df78c0
lvchunhui7 <lvchunhui7@126.com>
2017-09-28 09:38:32 +0800

    sub middleware

---
+++ dragon/middlewares.py
@@ -0,0 +1,11 @@
+#coding:utf-8
+'''
+Created on 2017年9月28日
+
+@author: lch
+'''
+class SubdomainMiddleware(object):
+    def process_request(self, request):
+        domain_parts = request.get_host().split('.')
+        if len(domain_parts) == 3 and domain_parts[0] != 'www':
+            request.path_info = '/%s/%s' % (domain_parts[0], request.path)
